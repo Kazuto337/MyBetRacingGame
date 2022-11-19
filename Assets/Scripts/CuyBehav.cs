798a1e5dc3e8e6197bbb842d1abc14307c4caa44
Kazuto337 <60271850+Kazuto337@users.noreply.github.com>
2022-11-18 22:38:08 -0500

    Uploading Project

---
+++ Assets/Scripts/CuyBehav.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class CuyBehav : MonoBehaviour
+{
+    [SerializeField] PathCreator pathCreator;
+    [SerializeField] float speed ,minSpeed , maxSpeed;
+    public bool canMove;
+    float travelledDistance;
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            travelledDistance += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(travelledDistance);
+        }        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("RotationObj"))
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(TurnOnCollider());
+            Quaternion rotation = transform.rotation;
+            rotation.z = collision.GetComponent<RotationObj>().RotationValue;
+            transform.Rotate(Vector3.forward, collision.GetComponent<RotationObj>().RotationValue);
+
+            Vector3 scale = transform.localScale;
+            scale.y = collision.GetComponent<RotationObj>().ScaleValue;
+            transform.localScale = scale;            
+        }
+        if (collision.CompareTag("FinishLine"))
+        {
+            print("b");
+            GameManager.instance.FinishRace(gameObject.name);
+        }
+        if (collision.CompareTag("Changer"))
+        {
+            speed = Random.Range(minSpeed , maxSpeed);
+        }
+    }
+
+    IEnumerator TurnOnCollider()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
